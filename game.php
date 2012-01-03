7b0286e870c09fb05377dba6d9ab2ea470e34b90
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-01-02 18:12:40 -0800

    move logic for game disply to seperate file

--- game.php
+++ game.php
@@ -23,35 +23,7 @@
         <input type="submit" value="Submit"/>
       </form>
       <pre id="game0001">
-      <!-- stoke to begin hacks HOMEY -->
-<?
-$db = new PDO('sqlite:chess.db');
-$result = $db->query('SELECT * FROM moves');
-$moveNumber = 0;
-$displayNumber = 0;
-foreach($result as $row)
-{
-  if ($row[0] & 1) {
-    $displayNumber = $displayNumber + 1;
-    print "\t  ".$displayNumber.". ".$row['move'];
-  }
-  else {
-    print " ".$row['move']."\n";
-
-  }
-  $moveNumber = $moveNumber + 1;
-}
-
-if (($_POST['move'])  ) {
-    $num = $moveNumber + 1;
-    $move = htmlspecialchars($_POST['move']);
-    $db->query("INSERT INTO moves (id, move) VALUES ('$num','$move')");
-    $db = NULL;
-    echo"<script>window.location.reload();</script>";
-}
-
-$db = NULL;
-?>
+      <? include('move.php'); ?>
       </pre>
 
       <div id="controls">
@@ -96,6 +68,7 @@ jQuery(function($) {
   catch(e) {
     $('#game').hide();
     $('#myErr').show();
+
     <?
     if (($_POST['undo'])) {
       $db = new PDO('sqlite:chess.db');
