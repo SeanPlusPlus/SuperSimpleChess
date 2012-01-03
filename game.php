eab8310ef374d4b064ecbd4c55f726463069884a
Kevin Rocci <kevinrocci@gmail.com>
2012-01-02 17:22:40 -0800

    change to comment

--- game.php
+++ game.php
@@ -23,7 +23,7 @@
         <input type="submit" value="Submit"/>
       </form>
       <pre id="game0001">
-      <!-- begin hacks -->
+      <!-- begin hacks yo -->
 <?
 $db = new PDO('sqlite:chess.db');
 $result = $db->query('SELECT * FROM moves');
