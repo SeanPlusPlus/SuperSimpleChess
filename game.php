90211bc529c732719d55daf1c5a7a20e2e299412
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-01-02 17:25:46 -0800

    update comment on game

--- game.php
+++ game.php
@@ -23,7 +23,7 @@
         <input type="submit" value="Submit"/>
       </form>
       <pre id="game0001">
-      <!-- begin hacks yo -->
+      <!-- begin hacks yo Dude -->
 <?
 $db = new PDO('sqlite:chess.db');
 $result = $db->query('SELECT * FROM moves');
