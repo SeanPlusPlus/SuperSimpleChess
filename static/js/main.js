f03b69caeeb507fac02bbb5518fa6cc95bcf53aa
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-05-27 17:43:16 -0700

    add function to cancel move and reset board

--- static/js/main.js
+++ static/js/main.js
@@ -1,62 +1,74 @@
 $( document ).ready( function() {
   $.getJSON( '/js/figures.json', function( data ) {
 
-    // draw the board
-    for ( rank = 8 ; rank > 0 ; rank-- ) {
-      var row = "<tr>";
-      if ( rank%2 == 0  ) {
-        var primary = "light";
-        var secondary = "dark";
-      }
-      else {
-        var primary = "dark";
-        var secondary = "light";
-      }
-      var files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-      for ( file = 0 ; file < 4 ; file++ ) {
-        row += "<td id='" + files[0] + rank + "'  class='" + primary + "'></td>";
-        files.splice( 0,1 );
-        row += "<td id='" + files[0] + rank + "' class='" + secondary + "'></td>";
-        files.splice( 0,1 );
-      }
-      row += "</tr>";
-      $( 'table' ).append( row );
-    }
+    drawBoard();
+    placeFigures();
+    moveFigures();
 
-    // place the figures on the board
-    $.each( data, function( position, figure ) {
-      $( '#' + position ).html(
-        "<a class='figure' href='#'>" + figure[0] + "</a>"
-      );
-    });
+    function drawBoard() {
+      $( 'tr' ).remove();
+      for ( rank = 8 ; rank > 0 ; rank-- ) {
+        var row = "<tr>";
+        if ( rank%2 == 0  ) {
+          var primary = "light";
+          var secondary = "dark";
+        }
+        else {
+          var primary = "dark";
+          var secondary = "light";
+        }
+        var files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        for ( file = 0 ; file < 4 ; file++ ) {
+          row += "<td id='" + files[0] + rank + "'  class='" + primary + "'></td>";
+          files.splice( 0,1 );
+          row += "<td id='" + files[0] + rank + "' class='" + secondary + "'></td>";
+          files.splice( 0,1 );
+        }
+        row += "</tr>";
+        $( 'table' ).append( row );
+      }
+    };
 
-    // move the figures around
-    var figureClicked = false,
-        clickCount = 0,
-        source,
-        destination;
-    $( 'td' ).click( function() {
-      // you first need to click a figure, and a <td> will
-      // only have a child element if there's a figure on it
-      if( $( this ).children().length  > 0 ){
-        figureClicked = true;
-      }
-      if( figureClicked && ( clickCount < 2 ) ) {
-        clickCount += 1;
-        $( this ).css( 'background-color','yellow' );
-        // source square
-        if ( clickCount == 1 ) {
-          source = $( this );
-          console.log( source );
+    function placeFigures() {
+      $.each( data, function( position, figure ) {
+        $( '#' + position ).html(
+          "<a class='figure' href='#'>" + figure[0] + "</a>"
+        );
+      });
+    };
+
+    function moveFigures() {
+      var figureClicked = false,
+          clickCount = 0,
+          source,
+          destination;
+      $( 'td' ).click( function() {
+        // you first need to click a figure, and a <td> will
+        // only have a child element if there's a figure on it
+        if( $( this ).children().length > 0 ){
+          figureClicked = true;
         }
-        // destination destination
-        if ( clickCount == 2 ) {
-          destination = $( this );
-          console.log( destination );
-          $( this ).html( source.children( 'a' ) );
-          source.html();
+        if( figureClicked && ( clickCount < 2 ) ) {
+          clickCount += 1;
+          $( this ).css( 'background-color','yellow' );
+          // source square
+          if ( clickCount == 1 ) {
+            source = $( this );
+          }
+          // destination square
+          if ( clickCount == 2 ) {
+            destination = $( this );
+            $( this ).html( source.children( 'a' ) );
+            source.html();
+          }
         }
-      }
+      });
+    };
+
+    $( '#cancelMove' ).click( function() {
+      drawBoard();
+      placeFigures();
+      moveFigures();
     });
   });
 });
